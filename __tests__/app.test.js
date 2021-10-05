18e31551aecad3743e1272b63697f99aa932292d
Pete Hamrick <pete.hamrick@gmail.com>
2021-10-05 11:57:12 -0700

    wrote test, controller, model for studio; still need setupdb and SQL

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -44,6 +44,18 @@ describe('ripe-banana routes', () => {
       });
   });
 
+  it('gets the studios', () => {
+    return request(app)
+      .get('/studios')
+      .then((res) => {
+        expect(res.body).toEqual([
+          { studio_id: expect.any(String), name: expect.any(String) },
+          { studio_id: expect.any(String), name: expect.any(String) },
+          { studio_id: expect.any(String), name: expect.any(String) },
+        ]);
+      });
+  });
+
   afterAll(() => {
     pool.end();
   });
