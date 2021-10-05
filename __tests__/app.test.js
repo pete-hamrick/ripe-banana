a6fb7404545c5cb752316770672771152e61c3f3
Pete Hamrick <pete.hamrick@gmail.com>
2021-10-05 11:03:30 -0700

    wrote reviewer controller and model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,45 @@
 const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
-// const request = require('supertest');
-// const app = require('../lib/app.js');
+const request = require('supertest');
+const app = require('../lib/app.js');
 
 describe('ripe-banana routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('should pass on github', () => {
-    expect(1).toEqual(1);
+  it('gets reviewer', () => {
+    return request(app)
+      .get('/reviewer')
+      .then((res) => {
+        expect(res.body).toEqual([
+          {
+            reviewerId: expect.any(String),
+            name: expect.any(String),
+            company: expect.any(String),
+          },
+          {
+            reviewerId: expect.any(String),
+            name: expect.any(String),
+            company: expect.any(String),
+          },
+          {
+            reviewerId: expect.any(String),
+            name: expect.any(String),
+            company: expect.any(String),
+          },
+          {
+            reviewerId: expect.any(String),
+            name: expect.any(String),
+            company: expect.any(String),
+          },
+          {
+            reviewerId: expect.any(String),
+            name: expect.any(String),
+            company: expect.any(String),
+          },
+        ]);
+      });
   });
 
   afterAll(() => {
